dcdd0492dff8809bb652f398c4f53886c5f2dc66
Timur Magomedov <timurmag2020@gmail.com>
2022-09-18 18:49:41 +0300

    подключил скрипт, добавил несколько функций

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// Does range include number 'to'?
+function getRandomNumber(from, to) {
+  if (from < 0 || to < 0) {
+    console.log('Диапазон не может быть отрицательным.');
+  }
+  const randomNumber = Math.floor(Math.random() * (Math.abs(to - from) + 1)) + from;
+  return randomNumber;
+}
+
+function checkMaxLength(checkingString, maxLength) {
+  return checkingString.length <= maxLength;
+}
