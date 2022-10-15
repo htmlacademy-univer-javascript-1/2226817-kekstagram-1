4796d62cefe624d5d7827d48b4a5974b70584eec
Timur Magomedov <timurmag2020@gmail.com>
2022-10-15 13:29:11 +0300

    8th lesson

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {createPosts} from './data.js';
 import {renderPicture} from './render.js';
-import {openWindow, bigPicture} from './view-window.js';
+import {openBigPicture} from './view-window.js';
+import './image-upload.js';
 
 const posts = createPosts();
 
@@ -15,8 +16,7 @@ const addCheckHandler = (post) => {
   const picture = pictures.querySelector(srcImage).parentNode;
   picture.addEventListener('click', (evt) => {
     evt.preventDefault();
-    document.body.classList.add('modal-open');
-    openWindow(post);
+    openBigPicture(post);
   });
 };
 
@@ -24,16 +24,3 @@ for (const post of posts) {
   addCheckHandler(post);
 }
 
-const closeButton = bigPicture.querySelector('.big-picture__cancel');
-
-closeButton.addEventListener('click', () => {
-  bigPicture.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-});
-
-document.addEventListener('keydown', (evt) => {
-  if (evt.key === 'Escape') {
-    bigPicture.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-  }
-});
