53faf2b39245022bb6cf2fa14d77a8fe376127ae
Keks <keks@htmlacademy.ru>
2022-11-04 18:17:30 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,14 @@
+import { rendering } from './render.js';
+import { getData } from './api.js';
+import { showAlert } from './util.js';
+import './image-upload.js';
+import './preview-file.js';
+
+getData((posts) => {
+  rendering(posts);
+},
+() => {
+  showAlert('Не удалось загрузить данные. Попробуйте перезагрузить страницу', 0);
+});
+
+
